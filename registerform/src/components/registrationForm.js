e1b37eba79270d0316a93d8e475213f35548e920
SAHIL ALI <84962462+SahilAli8808@users.noreply.github.com>
2022-10-31 17:01:30 +0530

    name email contact added

--- registerform/src/components/registrationForm.js
+++ registerform/src/components/registrationForm.js
@@ -5,24 +5,18 @@ function RegistrationForm() {
       <div className="form">
           <div className="form-body">
               <div className="username">
-                  <label className="form__label" for="firstName">First Name </label>
-                  <input className="form__input" type="text" id="firstName" placeholder="First Name"/>
+                  <label className="form__label" for="firstName">Name </label>
+                  <input className="form__input" type="text" id="firstName" placeholder="Your Name"/>
               </div>
-              <div className="lastname">
-                  <label className="form__label" for="lastName">Last Name </label>
-                  <input  type="text" name="" id="lastName"  className="form__input"placeholder="LastName"/>
-              </div>
+
               <div className="email">
                   <label className="form__label" for="email">Email </label>
-                  <input  type="email" id="email" className="form__input" placeholder="Email"/>
+                  <input  type="email" id="email" className="form__input" placeholder="Your Email"/>
               </div>
-              <div className="password">
-                  <label className="form__label" for="password">Password </label>
-                  <input className="form__input" type="password"  id="password" placeholder="Password"/>
-              </div>
-              <div className="confirm-password">
-                  <label className="form__label" for="confirmPassword">Confirm Password </label>
-                  <input className="form__input" type="password" id="confirmPassword" placeholder="Confirm Password"/>
+             
+              <div className="contact-number">
+                  <label className="form__label" for="confirmPassword">Contact Number </label>
+                  <input className="form__input" type="contact" id="contactNumber" placeholder="Enter Your Number"/>
               </div>
           </div>
           <div class="footer">
