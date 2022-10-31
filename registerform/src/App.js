3f04cb98958ce1b4a10e8b8c747bb1644b1708b6
SAHIL ALI <84962462+SahilAli8808@users.noreply.github.com>
2022-10-31 17:57:01 +0530

    added header

--- registerform/src/App.js
+++ registerform/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/header';
 import RegistrationForm from './components/registrationForm'
 
+
 function App() {
   return (
     <div className="App">
